be2c9629e5ab1ff6661feed4ddfbc53fcec25a73
tminhbao <tminhbao2001@gmail.com>
2022-10-07 15:48:03 +0700

    create header (index.html)

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ searchElement.onclick = function () {
 searchElement.onblur = function () {
   searchElement.style.width = "30px";
 };
+
+const sunIconElement = document.querySelector(".fa-sun");
+const moonIconElement = document.querySelector(".fa-moon");
+
+const changeTheme = function (x) {
+  x.classList.remove("fa-regular fa-moon toggle-theme-icon");
+  x.classList.add("fa-regular fa-sun toggle-theme-icon");
+};
+
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
